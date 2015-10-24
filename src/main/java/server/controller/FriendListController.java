108fd536b5ee5b60154cea3d51d4df4003046c7b
AnastasiaSulyagina <anastasia.sulyagina@gmail.com>
2015-10-24 18:05:49 +0300

    a bit of api added

---
+++ src/main/java/server/controller/FriendListController.java
@@ -0,0 +1,16 @@
+package server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class FriendListController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String getFriendList() {
+    }
+
+}
---
+++ src/main/java/server/controller/User.java
@@ -0,0 +1,9 @@
+package server.controller;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    public int id;
+    public String first_name;
+    public String last_name;
+}
